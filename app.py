03e229d0e426214784f681bc0a4789416b3fccf0
Ford Heacock <ford.heacock@smiledirectclub.com>
2019-12-17 14:34:14 -0600

    testing

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ TODO
 
 """
 
-############ LOGGING ############
+
 log = logging.getLogger('apscheduler.executors.default')
 log.setLevel(logging.INFO)  # DEBUG
 
@@ -31,14 +31,20 @@ fmt = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
 h = logging.StreamHandler()
 h.setFormatter(fmt)
 log.addHandler(h)
-############ LOGGING ############
 
 flask_app = Flask(__name__)
 
-# initialize scheduler with preferred timezone. Implements jobstore for persistance
+# initialize scheduler with your preferred timezone
 scheduler = BackgroundScheduler({'apscheduler.timezone': 'America/Chicago'}, jobstores={'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')})
 
+# add a custom jobstore to persist jobs across sessions (default is in-memory)
+# scheduler.add_jobstore('sqlalchemy', url='sqlite:////tmp/schedule.db')
 scheduler.start()
+
+####### EXAMPLE CURL CALL
+# curl -X POST http://127.0.0.1:5000/schedule-flight  -H 'content-type: application/json' -d '{"conf": "NFMIU4", "fname": "ford", "lname": "heacock"}'
+# curl -X POST https://pyschedule.herokuapp.com/schedule-flight  -H 'content-type: application/json' -d '{"conf": "NFMIU4", "fname": "ford", "lname": "heacock"}'
+# curl -X POST https://pyschedule.herokuapp.com/schedule-flight  -H 'content-type: application/json' -d '{"time":"2019-12-17T12:42", "conf": "NFO8SY", "fname": "Haley", "lname": "Heacock"}'
 
 @flask_app.route('/', methods=['GET'])
 def hello():
@@ -46,31 +52,33 @@ def hello():
 
 
 @flask_app.route('/schedule-flight', methods=['POST'])
-def schedule_flight_direct():
-    # bypasses form POST if wanted, can hit with curl for example:
-    # curl -X POST https://pyschedule.herokuapp.com/schedule-flight  -H 'content-type: application/json' -d '{"time":"2019-12-17T12:42", "conf": "ABC123", "fname": "Bob", "lname": "Saget"}
+def schedule_to_print():
     data = request.get_json()
-
+    #get time to schedule and text to print from the json
     time = data.get('time')
     conf = data.get('conf')
     fname = data.get('fname')
     lname = data.get('lname')
-    # convert to datetime
+    #convert to datetime
     date_time = datetime.datetime.strptime(str(time), '%Y-%m-%dT%H:%M')
-    # schedule job
+    #schedule the method 'printing_something' to run the the given 'date_time' with the args 'text'
     job = scheduler.add_job(auto_checkin, trigger='date', next_run_time=str(date_time),
-                            args=[conf, fname, lname])
+                            args=[conf, fname, lname], jobstore={'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite'))
     return "job details: %s" % job
+
+
+def printing_something(text):
+    print("printing %s at %s" % (text, datetime.datetime.now()))
 
 
 @flask_app.route('/schedule-flight-form', methods=['POST'])
 def schedule_flight():
     data = request.form
-    # get current time and add one minute to delay scheduled job
     now = datetime.datetime.now()
+
     now_plus_1 = now + datetime.timedelta(minutes = 1)
     now_plus_1 = now_plus_1.replace(second=0, microsecond=0)
-
+    #get time to schedule and text to print from the json
     conf = data.get('conf')
     fname = data.get('fname')
     lname = data.get('lname')
@@ -85,7 +93,11 @@ def schedule_flight():
         'scheduled_for': now_plus_1
     })
 
-
 @flask_app.route('/thanks', methods=['GET'])
 def thanks():
     return render_template('thanks.html')
+
+
+if __name__ == '__main__':
+    print(scheduler.get_all_jobs())
+    flask_app.run()
