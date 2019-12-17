8a98d0353e6fe9c40f3c864aba7aac28987748f9
Ford Heacock <ford.heacock@smiledirectclub.com>
2019-12-17 12:34:06 -0600

    yes

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ TODO
  -conf example: NFMIU4/NFO8SY
  -(it works without apscheduler)
 
- -try with AsyncIOScheduler?
 """
 
 
@@ -44,8 +43,8 @@ scheduler.start()
 
 ####### EXAMPLE CURL CALL
 # curl -X POST http://127.0.0.1:5000/schedule-flight  -H 'content-type: application/json' -d '{"conf": "NFMIU4", "fname": "ford", "lname": "heacock"}'
-# curl -X POST https://pyschedule.herokuapp.com/schedule-flight  -H 'content-type: application/json' -d '{"conf": "NFMIU4", "fname": "ford", "lname": "heacock"}' "time":"2019-12-16T14:47"
-#######
+# curl -X POST https://pyschedule.herokuapp.com/schedule-flight  -H 'content-type: application/json' -d '{"conf": "NFMIU4", "fname": "ford", "lname": "heacock"}'
+# curl -X POST https://pyschedule.herokuapp.com/schedule-flight  -H 'content-type: application/json' -d '{"time":"2019-12-17T12:33", "conf": "NFMIU4", "fname": "ford", "lname": "heacock"}'
 
 @flask_app.route('/', methods=['GET'])
 def hello():
@@ -56,13 +55,14 @@ def hello():
 def schedule_to_print():
     data = request.get_json()
     #get time to schedule and text to print from the json
-    time = data.get('time')
-    text = data.get('text')
+    conf = data.get('conf')
+    fname = data.get('fname')
+    lname = data.get('lname')
     #convert to datetime
     date_time = datetime.datetime.strptime(str(time), '%Y-%m-%dT%H:%M')
     #schedule the method 'printing_something' to run the the given 'date_time' with the args 'text'
-    job = scheduler.add_job(printing_something, trigger='date', next_run_time=str(date_time),
-                            args=[text])
+    job = scheduler.add_job(auto_checkin, trigger='date', next_run_time=str(date_time),
+                            args=[conf, fname, lname])
     return "job details: %s" % job
 
 
